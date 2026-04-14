7f36d8b4e336f35a21a7156a4a68423021d47e26
Vitalii Supryhan <2298102+Supryhan@users.noreply.github.com>
2026-04-14 08:51:44 +0300

    fix: fix version

--- src/test/scala/studentservice/config/StudentServiceConfigSpec.scala
+++ src/test/scala/studentservice/config/StudentServiceConfigSpec.scala
@@ -5,6 +5,6 @@ import munit.CatsEffectSuite
 class StudentServiceConfigSpec extends CatsEffectSuite:
   test("loads service HTTP settings from application.conf"):
     StudentServiceConfig.load.map: config =>
-      assertEquals(config.version, "0.2.0-SNAPSHOT")
+      assertEquals(config.version, "0.3.0-SNAPSHOT")
       assertEquals(config.http.host, "0.0.0.0")
       assertEquals(config.http.port, 8080)
